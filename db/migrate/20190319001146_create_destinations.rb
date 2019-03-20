38eeb9dbb80bbfd4227976a0e87cdfea1e2b0cba
EdselCaprice <caprice@mail.usf.edu>
2019-03-19 18:55:28 -0700

    update migrations

--- db/migrate/20190319001146_create_destinations.rb
+++ db/migrate/20190319001146_create_destinations.rb
@@ -1,7 +1,8 @@
 class CreateDestinations < ActiveRecord::Migration[5.2]
   def change
     create_table :destinations do |t|
-
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :experience, foreign_key: true
       t.timestamps
     end
   end
