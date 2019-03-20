115b4d1a678a34a363d575a17c9be3a89192c9d3
EdselCaprice <caprice@mail.usf.edu>
2019-03-19 19:09:45 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+
+    def add_wander_point
+        update_points(1)
+    end
+
+    def remove_wander_point
+        update_points(-1)
+    end
+
+    private
+
+    def update_points(amount)
+        self.user.update(wanderpoints: self.wanderpoints + amount)
+    end
+
+
+
 end
