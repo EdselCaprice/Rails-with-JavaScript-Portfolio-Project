01048b27333053df04930dff6eb86f7c7f2cabef
EdselCaprice <caprice@mail.usf.edu>
2019-03-20 16:25:41 -0700

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,24 @@
 class SessionsController < ApplicationController
+
   def create
+    @user = User.find_by(username: params[:user][:username])
+   if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+        redirect_to signin_path
+    end
   end
 
-  def new
-  end
+    def new 
+        @user = User.new
+        @users = User.all
+    end
 
-  def destroy
-  end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_url
+    end
+
 end
