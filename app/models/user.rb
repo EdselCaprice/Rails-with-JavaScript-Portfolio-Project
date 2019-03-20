9e107d8da5f7644b7804e1f81d0865a8c6d05a67
EdselCaprice <caprice@mail.usf.edu>
2019-03-19 19:40:56 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
     has_many :experiences, through: :destinations
     has_many :destinations
 
-    def add_wander_point
+    def add_wanderpoint
         update_points(1)
     end
 
-    def remove_wander_point
+    def remove_wanderpoint
         update_points(-1)
     end
 
