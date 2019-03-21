15b6c0ed15224774595b01905ed72934d5ae0781
EdselCaprice <caprice@mail.usf.edu>
2019-03-20 17:33:06 -0700

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 
     def index
+        @users = User.all
     end
 
     def show
+
     end
 
     def new
+        @user = User.new
     end
-    
+
     def edit
     end
 
@@ -16,6 +21,16 @@ class UsersController < ApplicationController
     end
 
     def update
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+      end
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
     end
 
 end
