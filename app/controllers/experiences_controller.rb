2d649e8699ede4942883bb183cb378e2716faf63
EdselCaprice <caprice@mail.usf.edu>
2019-03-21 15:40:25 -0700

    update destinations controller methods

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,20 +1,50 @@
 class ExperiencesController < ApplicationController
 
     def index
+        @experiences = Experience.all
     end
 
     def show
     end
 
     def new
+        @experience = Experience.new
     end
     
     def edit
     end
 
     def create
+        @user = Experience.new(experience_params)
+        if @experience.save
+            session[:experience_id] = @experience.id
+            redirect_to experience_path(@experience)
+        else
+            render :new
+        end
     end
 
     def update
+        if @experience.update(experience_params)
+            redirect_to @experience
+        else
+            render :edit
+        end
     end
+
+    def destroy
+        @experience.destroy
+        redirect_to users_url
+    end
+
+    private
+
+    def set_experience
+        @experience = Experience.find(params[:id])
+      end
+
+    def experience_params
+        params.require(:user).permit(:activity)
+    end
+
 end
