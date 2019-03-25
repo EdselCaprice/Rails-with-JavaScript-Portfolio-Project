4df2391b658798b22f64b881a184ea49172a598b
EdselCaprice <caprice@mail.usf.edu>
2019-03-24 20:04:26 -0700

    add validations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,4 +2,6 @@ class Destination < ApplicationRecord
     belongs_to :user
     has_many :experiences
     scope :locations, -> { order(:location) }
+    validates :location, presence: true
+
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,6 @@
 class Experience < ApplicationRecord
     has_many :users, through: :destinations
     belongs_to :destination
+    validates :activity, presence: true
+
 end
